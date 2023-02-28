2bd87171dd335618b2e3179c6a3acdca7efcfee2
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-28 16:57:59 -0600

    Added Task 1 instructions in algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,5 @@
+# Task 1: Even or Odd
+# Given a numeric value, determine if it is even or odd.
+# The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
+# Leave a comment above the function stating the time complexity.
+# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
