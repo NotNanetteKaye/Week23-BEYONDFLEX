643ee2297b77d2e1c2d72348b351f585cab0fea5
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 15:14:24 -0600

    Wrote logic for task #1 in algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -3,3 +3,9 @@
 # The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
 # Leave a comment above the function stating the time complexity.
 # Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+
+number = int(input('Please enter a number: '))
+if (number % 2 == 0):
+    print(number, ' is an even number')
+else:
+    print(number, ' is an odd number')
