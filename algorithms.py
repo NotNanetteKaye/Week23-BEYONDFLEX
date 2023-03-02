702471075fba5fd3c0aedb98afabef064bb3c74a
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-01 21:15:09 -0600

    🤔: need to make a boolean ex for T1 in algor.py

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,12 @@
 # Task 1: Even or Odd
-# Given a numeric value, determine if it is even or odd.
-# The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
-# Leave a comment above the function stating the time complexity.
-# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+    # Given a numeric value, determine if it is even or odd.
+    # The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
+    # Leave a comment above the function stating the time complexity.
+    # Send a screenshot of your solution and time complexity comment to your personal instructors chat.
 
+#boolean example: 
+
+# non-boolean example:
 number = int(input('Please enter a number: '))
 if (number % 2 == 0):
     print(number, ' is an even number')
@@ -12,7 +15,7 @@ else:
 
 
 # Task 2: Less than 100
-# Given a list of numbers, determine if each item in the list is lower than 100.
-# The function should take in the list as a parameter and return a boolean value (False if there are any numbers greater than or equal to 100, True if all numbers are less than 100).
-# Leave a comment above the function stating the time complexity.
-# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+    # Given a list of numbers, determine if each item in the list is lower than 100.
+    # The function should take in the list as a parameter and return a boolean value (False if there are any numbers greater than or equal to 100, True if all numbers are less than 100).
+    # Leave a comment above the function stating the time complexity.
+    # Send a screenshot of your solution and time complexity comment to your personal instructors chat.
