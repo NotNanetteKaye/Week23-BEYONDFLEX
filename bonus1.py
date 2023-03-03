1a33748f775e4921012587f07cc49f541b7ffd30
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-02 19:17:08 -0600

    Comment about nested loops logic in bonus1.py

--- bonus1.py
+++ bonus1.py
@@ -5,3 +5,7 @@ for row in range(0, rows):
     for column in range(row):
         print('#', end='')
     print('')
+
+# To define rows and columns in python to print patterns, you need to use a nested loop.
+# The outer loop iterates the number of rows. 
+# The inner loop iterates the number of columns.
