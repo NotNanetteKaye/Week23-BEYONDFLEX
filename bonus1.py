8291a7cd9dba452049accd2dba48e8b6de6a31e6
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-02 19:18:19 -0600

    🤔 solve task 2 in bonus1.py

--- bonus1.py
+++ bonus1.py
@@ -9,3 +9,5 @@ for row in range(0, rows):
 # To define rows and columns in python to print patterns, you need to use a nested loop.
 # The outer loop iterates the number of rows. 
 # The inner loop iterates the number of columns.
+
+# Task 2: Write a program that will print the following to the console using the print() function.
